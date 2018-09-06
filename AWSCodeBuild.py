e85a81a2cd4cce359a4db66a1a3d2c1e29454575
renesobral <renesobral@hotmail.com>
2018-09-06 18:39:37 -0400

    Python Script to Deploy WebSite Lambda3

--- AWSCodeBuild.py
+++ AWSCodeBuild.py
@@ -8,7 +8,20 @@ from botocore.client import Config
 
 def lambda_handler(event, context):
     objT = AWSMoveProduction()
-    objT.run()
+    try:
+        objT.run(event)
+    except:
+        objT.publish("Error on deployment:")
+        raise
+
+
+    objT.publish("Sucess deployed")
+    job = event.get('CodePipeline.job')
+
+    if job:
+        codepipeline = boto3.client('codepipeline')
+        codepipeline.put_job_sucess_result(jobId=job["id"])
+
     return {
         "statusCode": 200,
         "body": json.dumps('Hello from Lambda!')
@@ -18,12 +31,27 @@ class AWSMoveProduction(object):
     def __init__(self):
         pass
 
-    def run(self):
+    def publish(self, message):
+        sns = boto3.resource('sns')
+        topic = sns.Topic('arn:aws:sns:us-east-1:572667294264:DeployBuild')
+        topic.publish(Subject='Build Finshed', Message=message)
+
+    def run(self, event):
+
+        job = event.get('CodePipeline.job')
+        location = { "bucketName": 'renesobral.build', "objectKey": "mycode.zip"}
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MuAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building from: " + str(location)
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-        my_git_bucket = s3.Bucket('renesobral.build')
+        my_git_bucket = s3.Bucket(location["bucketName"])
         my_publish =  s3.Bucket('renesobral')
         file_men = StringIO.StringIO()
-        my_git_bucket.download_fileobj('mycode.zip', file_men)
+        my_git_bucket.download_fileobj(location["objectKey"], file_men)
 
         with zipfile.ZipFile(file_men) as myzip:
             for file_name in myzip.namelist():
